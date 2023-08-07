ed6f7e1bba7ad6a58740dd48cf049d700b704cb1
Martin Capodici <martin.capodici@gmail.com>
2023-08-07 18:30:31 +1000

    bugfix

--- model.py
+++ model.py
@@ -120,7 +120,6 @@ class CausalSelfAttention(nn.Module):
                 att = softmax1(att, dim=-1)
             else:
                 att = F.softmax(att, dim=-1)
-            att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
